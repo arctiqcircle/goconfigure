345d80c6a6be7d25a1dec80a2edf6881c1032d36
kennethgrace <kennethjeffersongrace@gmail.com>
2022-06-20 14:22:03 -0700

    no JSON and CSV handle testing

--- tests/Handler_test.go
+++ tests/Handler_test.go
@@ -12,6 +12,7 @@ import (
 )
 
 func TestConnectHandler(t *testing.T) {
+	t.Log("Testing connections from YAML inventory")
 	inv, err := inventory.LoadFromYAML("secrets/hosts.yml")
 	if err != nil {
 		panic(err)
@@ -19,7 +20,28 @@ func TestConnectHandler(t *testing.T) {
 	// BasicConnect to Hosts
 	var handlers []client.Handler
 	for _, host := range inv.Hosts {
-		t.Logf("Connectiong to %s", host.Hostname)
+		t.Logf("Connecting to %s", host.Hostname)
+		h, err := client.BasicConnect(host)
+		handlers = append(handlers, h)
+		if err != nil {
+			panic(err)
+		}
+	}
+	// Cleanup
+	for _, h := range handlers {
+		if err := h.Close(); err != nil {
+			log.Fatal("handlers failed to close properly", err)
+		}
+	}
+	t.Log("Testing connections from CSV inventory")
+	inv, err = inventory.LoadFromCSV("secrets/hosts.csv")
+	if err != nil {
+		panic(err)
+	}
+	// BasicConnect to Hosts
+	handlers = []client.Handler{}
+	for _, host := range inv.Hosts {
+		t.Logf("Connecting to %s", host.Hostname)
 		h, err := client.BasicConnect(host)
 		handlers = append(handlers, h)
 		if err != nil {
@@ -34,7 +56,8 @@ func TestConnectHandler(t *testing.T) {
 	}
 }
 
-func TestSendHandler(t *testing.T) {
+func TestEchoHandler(t *testing.T) {
+	t.Log("Testing connections from YAML inventory")
 	inv, err := inventory.LoadFromYAML("secrets/hosts.yml")
 	if err != nil {
 		panic(err)
@@ -42,7 +65,41 @@ func TestSendHandler(t *testing.T) {
 	// BasicConnect to Hosts
 	var handlers []client.Handler
 	for _, host := range inv.Hosts {
-		t.Logf("Connectiong to %s", host.Hostname)
+		t.Logf("Connecting to %s", host.Hostname)
+		h, err := client.BasicConnect(host)
+		handlers = append(handlers, h)
+		if err != nil {
+			panic(err)
+		}
+	}
+	// Send Command to Host
+	for _, h := range handlers {
+		response, err := h.Send("echo \"hello world!\"")
+		if err != nil {
+			panic(err)
+		}
+		response = strings.TrimSpace(response)
+		if response != "hello world!" {
+			panic(errors.New(fmt.Sprintf("response %s not equal to %s", response, "hello world!")))
+		} else {
+			t.Logf("response %s succesfully matches %s", response, "hello world!")
+		}
+	}
+	// Cleanup
+	for _, h := range handlers {
+		if err := h.Close(); err != nil {
+			log.Fatal("handlers failed to close properly", err)
+		}
+	}
+	t.Log("Testing connections from CSV inventory")
+	inv, err = inventory.LoadFromCSV("secrets/hosts.csv")
+	if err != nil {
+		panic(err)
+	}
+	// BasicConnect to Hosts
+	handlers = []client.Handler{}
+	for _, host := range inv.Hosts {
+		t.Logf("Connecting to %s", host.Hostname)
 		h, err := client.BasicConnect(host)
 		handlers = append(handlers, h)
 		if err != nil {
@@ -70,7 +127,8 @@ func TestSendHandler(t *testing.T) {
 	}
 }
 
-func TestMultiSendHandler(t *testing.T) {
+func TestTemplateHandler(t *testing.T) {
+	t.Log("Testing connections from YAML inventory")
 	inv, err := inventory.LoadFromYAML("secrets/hosts.yml")
 	tplString, err := render.FileToString("secrets/example.txt")
 	if err != nil {
@@ -80,7 +138,7 @@ func TestMultiSendHandler(t *testing.T) {
 	var handlers []client.Handler
 	var tpls [][]string
 	for _, host := range inv.Hosts {
-		t.Logf("Connectiong to %s", host.Hostname)
+		t.Logf("Connecting to %s", host.Hostname)
 		h, err := client.BasicConnect(host)
 		if err != nil {
 			panic(err)
@@ -96,7 +154,39 @@ func TestMultiSendHandler(t *testing.T) {
 				panic(err)
 			}
 			response = strings.TrimSpace(response)
-			fmt.Println(response)
+		}
+	}
+	// Cleanup
+	for _, h := range handlers {
+		if err := h.Close(); err != nil {
+			log.Fatal("handlers failed to close properly", err)
+		}
+	}
+	t.Log("Testing connections from CSV inventory")
+	inv, err = inventory.LoadFromCSV("secrets/hosts.csv")
+	if err != nil {
+		panic(err)
+	}
+	// BasicConnect to Hosts and Render Template
+	handlers = []client.Handler{}
+	tpls = [][]string{{}}
+	for _, host := range inv.Hosts {
+		t.Logf("Connecting to %s", host.Hostname)
+		h, err := client.BasicConnect(host)
+		if err != nil {
+			panic(err)
+		}
+		handlers = append(handlers, h)
+		tpls = append(tpls, render.RenderCommands(host.Data, tplString))
+	}
+	// Send Commands to Host
+	for i, h := range handlers {
+		for _, command := range tpls[i] {
+			response, err := h.Send(command)
+			if err != nil {
+				panic(err)
+			}
+			response = strings.TrimSpace(response)
 		}
 	}
 	// Cleanup
